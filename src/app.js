803003268238d9bdb6e95e4da76f15cd21634e6b
itshenrique <its.henrique@hotmail.com>
2021-02-21 18:08:50 -0300

    Ajuste

--- src/app.js
+++ src/app.js
@@ -42,7 +42,7 @@ app.use((err, req, res, next) => {
 });
 
 // configure port and listen for requests
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 export const server = app.listen(port, () => {
   log(`Server is running on http://localhost:${port} `);
